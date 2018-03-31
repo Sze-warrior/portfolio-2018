bed086b23cfcd44aaf3bcda1deadc36f8ff2410f
Michael Szeto <szetomj@gmail.com>
2018-04-01 11:17:48 +1200

    Fixed errors and CSS
    Fixed the following:
    
    Cache busting
    Mixins
    Video iframe responsiveness
    Animation for header mobile arrow
    Images and video content

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-var BtnmobileMenu = document.querySelector(".header-mobile-menu");
+var mobileMenu = document.querySelector(".header-mobile-menu");
 var navItems = document.querySelector(".header-nav-items-container");
+
 var lastX = window.innerWidth;
 var lastY = window.innerHeight;
 
+var mobileArrow = document.querySelector(".arrow");
 
+//Function to hide and show the navbar on mobile and rotate the arrow 
 function toggleMobileNav(e){
-	if(navItems.classList.contains("header-nav-items-container")){
+	if(mobileMenu){
 		navItems.classList.toggle("active");
+		mobileArrow.classList.toggle("rotated");
 	}
 }
 
-BtnmobileMenu.addEventListener("click", toggleMobileNav, false);
+mobileMenu.addEventListener("click", toggleMobileNav, false);
 
-function OnResize() {
 
+//Function to show navbar if user is resizing browser and toggled off the navbar
+function OnResize(e) {
    if (window.innerWidth <= 768) {
 		navItems.classList.remove("active");
    }
 }
 
 window.addEventListener("resize", OnResize, false);
+
